940de8d571eed2472c46ecef40b2562632dfe1a4
Annabelle Kennedy <queekusme@users.noreply.github.com>
2021-08-01 22:36:24 +0100

    Mild Formatting

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,17 @@
 import * as fs from "fs-extra";
 import * as path from "path";
-
-import { Command, OptionValues } from "commander";
+import * as commander from "commander";
 
 import Templates from "./Templates";
 import ProjectBuilder from "./ProjectBuilder";
 
-const program = new Command();
+const program: commander.Command = new commander.Command();
 
 (async() => {
     program
         .version(JSON.parse((await fs.readFile(path.join(process.cwd(), "package.json"))).toString()).version, "-v, --version", "output the current version")
         .argument("<project_name>", "name of the project")
-        //.argument('[additional...]', 'additional packages to install, dev packages are prefixed with D_ e.g. D_packagename')
-        .action(async (project_name: string/* , additional: string[] */, options: OptionValues) =>
+        .action(async (project_name: string, options: commander.OptionValues) =>
         {
             // Collect Options
             const optionsToLoad: string[] = Object.keys(options)
@@ -22,7 +20,6 @@ const program = new Command();
 
             await ProjectBuilder
                 .create(project_name)
-                //.additionalPackages(additional)
                 .templateParts([
                     ...(await Templates.getParts(Templates.required_Templates)),
                     ...(await Templates.getParts(optionsToLoad))
